656e35dc85e711640679e311d9328c93106d49fe
tfraven <sajiidx@gmail.com>
2024-09-02 16:58:36 +0500

    1. Project Setup

---
+++ jobwiz-backend/server.js
@@ -0,0 +1,26 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello, MERN Stack!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
